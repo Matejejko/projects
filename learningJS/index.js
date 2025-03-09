1993a405d0e52ddcedb8cfbc24736f2c32a8c263
Matejejko <155886081+Matejejko@users.noreply.github.com>
2025-03-09 18:29:51 +0100

    idk man i feel like dying

---
+++ learningJS/index.js
@@ -0,0 +1,17 @@
+//console.log("Hello World!");
+
+/*
+
+this shit a alert:
+
+window.alert("alert!");
+
+*/
+
+//comment for ngrrs
+
+/* 
+this is a multi line comment
+*/
+
+document.getElementById("nameH1").textContent = "Hello!";
